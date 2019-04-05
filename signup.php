609a0a1402453593f71d831f432c0340b74a221b
rangelo1102 <49273899+rangelo1102@users.noreply.github.com>
2019-04-05 20:44:16 +0800

    hnngngngngngnn

--- signup.php
+++ signup.php
@@ -1,4 +1,6 @@
 <?php
+//SIGN UP PAGE PHP CODE; NOT YET FINAL
+
 session_start();
 //credentials and stuff
 $host = "localhost";
@@ -7,19 +9,16 @@ $pass = "";
 $db ="tinders";
 
 // initialize
-$uname = '';
-$pword = '';
-$cPass = '';
-
+$uname = $_POST['newUser'];
+$pword = $_POST['newPass'];
+$cPass = $_POST['confirmPass'];
 
 // connect to the database
 $conn = new mysqli($host,$user,$pass,$db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$uname = filter_input(INPUT_POST, 'newUser');
-$pword = filter_input(INPUT_POST, 'newPass');
-$cPass = filter_input(INPUT_POST, 'confirmPass');;
+
 //CHECK IF ANY ERRORS
 if (isset($_POST['registerUser'])) {
 	if (empty($uname)) { 
@@ -58,19 +57,29 @@ if (isset($_POST['registerUser'])) {
 	";
 	}
   
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
-
-	/*
-  $user_check_query = "SELECT * FROM users WHERE username='$uname'";
+   $user_check_query = "SELECT * FROM users WHERE username='$uname' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
   if ($user) { // if user exists
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
-	}
+    }
   }
- */
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($pword);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (username, email, password) 
+  			  VALUES('$uname', ''$pword')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $uname;
+  	$_SESSION['success'] = "You are now logged in";
+  	echo "<script type='text/javascript'>
+		window.confirm('ok cool ur input's good n all);
+		window.location.href = 'Home.php';
+		</script>
+  }
 }
 ?>
